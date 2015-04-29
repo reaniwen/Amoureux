ce8ed4a49005842b49b962d07f8217e1c5c6ab8a
hongyegong <hg1068@nyu.edu>
2015-04-29 00:05:58 -0400

    Login

--- Amoureux/ViewController.swift
+++ Amoureux/ViewController.swift
@@ -20,6 +20,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let item1 = RMParallaxItem(image: UIImage(named: "item1")!, text: "SHARE LIGHTBOXES WITH YOUR TEAM")
+        let item2 = RMParallaxItem(image: UIImage(named: "item2")!, text: "FOLLOW WORLD CLASS PHOTOGRAPHERS")
+        let item3 = RMParallaxItem(image: UIImage(named: "item3")!, text: "EXPLORE OUR COLLECTION BY CATEGORY")
+        
+        let rmParallaxViewController = RMParallax(items: [item1, item2, item3], motion: false)
+        rmParallaxViewController.completionHandler = {
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                rmParallaxViewController.view.alpha = 0.0
+            })
+        }
+        
+        // Adding parallax view controller.
+        self.addChildViewController(rmParallaxViewController)
+        self.view.addSubview(rmParallaxViewController.view)
+        rmParallaxViewController.didMoveToParentViewController(self)
+        
         // Do any additional setup after loading the view, typically from a nib.
         
         let theWidth = view.frame.size.width
@@ -85,6 +101,10 @@ class ViewController: UIViewController {
             
         }
         
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
